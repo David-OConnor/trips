05708636688f2cf8c5253b040bfa2bed92f2a1f3
David <david.alan.oconnor@gmail.com>
2015-11-11 14:30:38 +0000

    more heroku troubleshooting

--- trips/wsgi.py
+++ trips/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trips.settings")
+##
+#application = get_wsgi_application()
+
+
+
+
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "squadron.settings")
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trips.settings")
+application = Cling(get_wsgi_application())
 
-application = get_wsgi_application()
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
